35c65dbedc1d6f4a298a73d9e76b3378c0414b8f
Abirami Sampathkumar <abiramisa@maveric-systems.com>
2021-10-14 11:24:37 +0530

    change to jenkin

--- src/main/java/com/selenium/sample/OrangeHRM.java
+++ src/main/java/com/selenium/sample/OrangeHRM.java
@@ -1,12 +1,6 @@
 package com.selenium.sample;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
